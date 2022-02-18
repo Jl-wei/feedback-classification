36591ddf6dd68a9e19f0a67361bcaa9069a273ac
Jialiang WEI <jialiang.wei@outlook.com>
2022-02-18 11:21:47 +0100

    add data preprocessing for P3

---
+++ data/P3-Golden-preprocess.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_excel('P3-Golden.xlsx')
+
+df['Judgement'] = df['labels'].replace(['none', 'Stability', 'quality', 'performance', 'feature'],
+                        [0, 1, 2, 3, 4], inplace=False)
+
+df.to_excel('P3-Golden.xlsx', index=False)
